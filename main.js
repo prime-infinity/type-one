77687c6adc76e5876f062796970e2b4a5c15311c
prime-infinity <soe.a.prime@gmail.com>
2023-07-26 07:12:07 -0700

    visualising from csv

--- main.js
+++ main.js
@@ -1,60 +1,89 @@
 import * as d3 from "d3";
 
-// Sample data for the bar chart
-const data = [
-  { label: "A", value: 10 },
-  { label: "B", value: 20 },
-  { label: "C", value: 15 },
-  { label: "D", value: 25 },
-  { label: "E", value: 12 },
-];
+// Function to load and display the CSV data
+async function loadCSV() {
+  try {
+    // Load the CSV file using D3.js
+    const csvData = await d3.csv("/Data/github-ranking.csv");
 
-// Set the dimensions and margins for the chart
-const margin = { top: 30, right: 30, bottom: 70, left: 60 };
-const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+    // Sort the data by stars in descending order
+    csvData.sort((a, b) => b.stars - a.stars);
 
-// Create the SVG container
-const svg = d3
-  .select("#chart-container")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    // Get the top 10 items
+    const top10Data = csvData.slice(0, 10);
 
-// Create the x and y scales
-const xScale = d3
-  .scaleBand()
-  .domain(data.map((d) => d.label))
-  .range([0, width])
-  .padding(0.1);
+    // Create the bar chart
+    createBarChart(top10Data);
 
-const yScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, (d) => d.value)])
-  .range([height, 0]);
+    // Display the data in the console
+    //console.log(csvData);
+  } catch (error) {
+    console.error("Error loading CSV:", error.message);
+  }
+}
 
-// Add the bars to the chart
-svg
-  .selectAll(".bar")
-  .data(data)
-  .enter()
-  .append("rect")
-  .attr("class", "bar")
-  .attr("x", (d) => xScale(d.label))
-  .attr("y", (d) => yScale(d.value))
-  .attr("width", xScale.bandwidth())
-  .attr("height", (d) => height - yScale(d.value));
+// Function to create the bar chart
+function createBarChart(data) {
+  // Set up the chart dimensions
+  const width = 800;
+  const height = 500;
+  const margin = { top: 40, right: 40, bottom: 60, left: 60 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
-// Add the x and y axis
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
+  // Create the SVG container
+  const svg = d3
+    .select("#chart-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-svg
-  .append("g")
-  .attr("class", "x-axis")
-  .attr("transform", `translate(0, ${height})`)
-  .call(xAxis);
+  // Create the x-scale for the stars
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.repo_name))
+    .range([0, innerWidth])
+    .padding(0.1);
 
-svg.append("g").attr("class", "y-axis").call(yAxis);
+  // Create the y-scale for the stars
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.stars)])
+    .range([innerHeight, 0]);
+
+  // Create the x-axis
+  const xAxis = d3.axisBottom(xScale);
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${height - margin.bottom})`)
+    .call(xAxis)
+    .selectAll("text")
+    .attr("transform", "rotate(-45)")
+    .attr("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em");
+
+  // Create the y-axis
+  const yAxis = d3.axisLeft(yScale);
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  // Create the bars
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d) => xScale(d.repo_name))
+    .attr("y", (d) => yScale(d.stars))
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => innerHeight - yScale(d.stars))
+    .attr("fill", "steelblue");
+}
+
+// Call the function to load and display the CSV data
+loadCSV();
