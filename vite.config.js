1433700d14c7ac936fc9a62cfc3a48f8fd2c7d78
prime-infinity <soe.a.prime@gmail.com>
2023-08-09 03:50:58 -0700

    vite config for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,16 @@
-export default {};
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // ... other Vite config options ...
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+
+  // Copy public folder to the root of the build output
+  publicDir: "public",
+});
